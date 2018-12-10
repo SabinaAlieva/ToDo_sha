4256cee7e49ec3ebe193331669531c707ae4848a
SabinaAlieva <sabina-alieva.taganrog@mail.ru>
2018-12-10 15:30:35 +0300

    Initial commit

---
+++ ToDo/Models/Task.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace ToDo.Models
+{
+    public class Task : INotifyPropertyChanged
+    {
+        private string _taskTitle;
+        private bool _isDone;
+        private string _taskNote;
+        private RelayCommand _addSubTask;
+        private string _newSubTaskName;
+        private SubTask _selectedSubTask;
+        private ObservableCollection<SubTask> _subTasks;
+
+        public Task(string title, Action<Task> deleteTask, Action<Task> updateTask)
+        {
+            DeleteTask = new RelayCommand(_ => deleteTask(this), _ => true);
+            UpdateTask = new RelayCommand(_ => updateTask(this), _ => true);
+            TaskTitle = title;
+            SubTasks = new ObservableCollection<SubTask>();
+        }
+
+        public int Id { get; set; }
+
+        public string TaskTitle
+        {
+            get => _taskTitle;
+            set
+            {
+                _taskTitle = value;
+                OnPropertyChanged("TaskTitle");
+            }
+        }
+
+        public bool IsDone
+        {
+            get => _isDone;
+            set
+            {
+                _isDone = value;
+                OnPropertyChanged("IsDone");
+            }
+        }
+
+        public string TaskNote
+        {
+            get => _taskNote;
+            set
+            {
+                _taskNote = value;
+                OnPropertyChanged("TaskNote");
+            }
+        }
+
+        public RelayCommand DeleteTask
+        {
+            get; private set;
+        }
+
+        public RelayCommand UpdateTask
+        {
+            get; private set;
+        }
+
+        public RelayCommand AddSubTask
+        {
+            get {
+                return _addSubTask ?? (_addSubTask = new RelayCommand(
+                    obj => {
+                        SubTask newSubTask = new SubTask(NewSubTaskName, t => SubTasks.Remove(t), _ => UpdateTask.Execute(null));
+                        SubTasks.Add(newSubTask);
+                        SelectedSubTask = newSubTask;
+                        NewSubTaskName = string.Empty;
+                        UpdateTask.Execute(null);
+                    }, _ => !(string.IsNullOrWhiteSpace(NewSubTaskName))));
+            }
+        }
+        public string NewSubTaskName
+        {
+            get => _newSubTaskName;
+            set
+            {
+                _newSubTaskName = value;
+                OnPropertyChanged("NewSubTaskName");
+            }
+        }
+
+        public SubTask SelectedSubTask
+        {
+            get => _selectedSubTask;
+            set
+            {
+                _selectedSubTask = value;
+                OnPropertyChanged("SelectedSubTask");
+            }
+        }
+
+        public ObservableCollection<SubTask> SubTasks { get => _subTasks; set => _subTasks = value; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ ToDo/Models/TaskList.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+using System;
+
+namespace ToDo.Models
+{
+    public class TaskList : INotifyPropertyChanged
+    {
+        private Task _selectedTask;
+        private string _taskListTitle;
+        private ObservableCollection<Task> _tasks;
+
+        public TaskList(string title, Action<TaskList> deleteTaskList)
+        {
+            DeleteTaskList = new RelayCommand(_ => deleteTaskList(this), _ => true);
+            TaskListTitle = title;
+            Tasks = new ObservableCollection<Task>();
+        }
+
+        public string TaskListTitle
+        {
+            get { return _taskListTitle; }
+            set
+            {
+                _taskListTitle = value;
+                OnPropertyChanged("TaskListTitle");
+                Console.WriteLine(value);
+            }
+        }
+
+        public ObservableCollection<Task> Tasks { get => _tasks; set => _tasks = value; }
+
+        public Task SelectedTask
+        {
+            get => _selectedTask;
+            set
+            {
+                _selectedTask = value;
+                OnPropertyChanged("SelectedTask");
+            }
+        }
+
+        public RelayCommand DeleteTaskList
+        {
+            get; private set;
+        }
+
+        public int Id { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
